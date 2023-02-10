ce6b16fd361387e0cf35184f9aee2eb7bab7cc5f
Mykola Shakh <mshah@tsukat.com>
2023-02-10 13:52:40 +0200

    add python packages as dependensies to

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kewlbear/Open3D-iOS", branch: "292cadc677494ed0c1b6c500996cd25525e5b46d"),
+        .package(url: "https://github.com/pvieito/PythonKit.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
