3d1bed725c3753f12805a99d52abbcaa006c445a
Mykola Shakh <mshah@tsukat.com>
2023-02-13 19:29:24 +0200

    add init open3d.

--- Sources/DeepFacePackage/DeepFace.swift
+++ Sources/DeepFacePackage/DeepFace.swift
@@ -1,7 +1,19 @@
+import Open3DSupport
+import NumPySupport
+import PythonSupport
+import PythonKit
+
 public struct DeepFace {
     public init() {
     }
     public static func test() {
         print("........ test from deepface package ........")
+    }
+    
+    public static func initOpen3d() {
+        PythonSupport.initialize()
+        Open3DSupport.sitePackagesURL.insertPythonPath()
+        NumPySupport.sitePackagesURL.insertPythonPath()
+        print("open3d init - done")
     }
 }
